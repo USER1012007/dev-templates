4f9ae64d37de06118a9b8b32b233fab6012c92b3
User1012007 <rojasbadilloe@gmail.com>
2025-11-02 15:30:51 -0600

    cpp: add template for cpp including .cpp base code

---
+++ cpp/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                packages =
+                  with pkgs;
+                  [
+                    clang-tools
+                    codespell
+                  ]
+                  ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+              };
+        }
+      );
+    };
+}
